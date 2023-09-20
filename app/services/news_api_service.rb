d90c342bc4b359da9267f4e9797f6277fdc1330e
hiroya0 <2000shigehiro0401@au.com>
2023-09-20 23:32:31 +0900

    Add:top-headlinesにて記事を取得

--- app/services/news_api_service.rb
+++ app/services/news_api_service.rb
@@ -4,7 +4,7 @@ class NewsApiService
   BASE_URL = "https://newsapi.org/v2/top-headlines"
 
   def self.fetch_car_news(country: "jp")
-    response = HTTParty.get(BASE_URL, query: { country: country, q: "トヨタ", apiKey: ENV['NEWS_API_KEY'] })
+    response = HTTParty.get(BASE_URL, query: { country: country, q: "車", apiKey: ENV['NEWS_API_KEY'] })
     response.parsed_response
   end
 end
