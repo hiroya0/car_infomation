1e881b19b43ef7a6a4c8af584f050182595c100f
hiroya0 <2000shigehiro0401@au.com>
2023-09-21 21:47:48 +0900

    Update:everythingに変更し、より多くの記事を取得

--- app/services/news_api_service.rb
+++ app/services/news_api_service.rb
@@ -1,10 +1,10 @@
 require 'httparty'
 
 class NewsApiService
-  BASE_URL = "https://newsapi.org/v2/top-headlines"
+  BASE_URL = "https://newsapi.org/v2/everything"
 
-  def self.fetch_car_news(country: "jp")
-    response = HTTParty.get(BASE_URL, query: { country: country, q: "車", apiKey: ENV['NEWS_API_KEY'] })
+  def self.fetch_car_news(language: "jp")
+    response = HTTParty.get(BASE_URL, query: { q: "車", language: language, apiKey: ENV['NEWS_API_KEY'] })
     response.parsed_response
   end
 end
