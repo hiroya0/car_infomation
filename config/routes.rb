ae4d86d305d2748ac432a1ffd8c69af6204b3aa8
hiroya0 <2000shigehiro0401@au.com>
2023-09-26 00:33:05 +0900

    Fix:deviseでログアウトできないエラーを改善

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root 'homes#index'
   
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :articles, only: [:index, :show]
 end
