f539349fa8d5430bc3c30091ec4f9602d92f27ac
hiroya0 <2000shigehiro0401@au.com>
2023-10-04 13:57:49 +0900

    Fix:ブックマークの削除機能を修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,31 +3,32 @@ class BookmarksController < ApplicationController
   include ApplicationHelper
 
   def create
-    puts params
-    article = Article.find_or_create_by(hashed_url: params[:article_hashed_url]) do |article|
-      
-      article.title = params[:title]
-      article.content = params[:content]
-      article.urlToImage = params[:urlToImage]
-    end
-
-    bookmark = current_user.bookmarks.build(article: article) 
+    hashed_url = params[:article_hashed_url]
+    @article = Article.find_or_initialize_by(hashed_url: hashed_url)
+    bookmark = Bookmark.find_or_create_by(user: current_user, article: @article)
+    Rails.logger.debug "Hashed URL: #{hashed_url}"
+    Rails.logger.debug "New Record?: #{@article.new_record?}"
     
-    if bookmark.save
-      flash[:notice] = 'ブックマーク登録しました'
-      redirect_to articles_path
+    if @article.new_record?
+      @article.assign_attributes(title: params[:title], content: params[:content], urlToImage: params[:urlToImage])
+      if @article.save
+        flash[:success] = 'Article was successfully bookmarked.'
+      else
+        flash[:error] = 'There was an error bookmarking the article.'
+      end
     else
-      puts bookmark.errors.full_messages
-      flash[:alert] = 'ブックマークに失敗しました'
-      redirect_to articles_path
+      flash[:notice] = 'Article is already bookmarked.'
     end
-  end
+    redirect_to articles_path 
+end
+
 
   def destroy
-    bookmark = current_user.bookmarks.find_by(article: Article.find_by(hashed_url: params[:article_hashed_url]))
+    bookmark = current_user.bookmarks.find(params[:id])
+    Rails.logger.debug "Deleting bookmark: #{bookmark.inspect}"
     bookmark&.destroy
     flash[:notice] = 'ブックマークを削除しました'
-    redirect_to articles_path
+    redirect_to bookmarks_path
   end
 
   def index
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   has_many :bookmarks
   has_many :users, through: :bookmarks
+  validates :hashed_url, uniqueness: true
 end
