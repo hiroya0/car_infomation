a4fb7c3c26ffdff0a8004ad7ddb52fee95811c69
hiroya0 <2000shigehiro0401@au.com>
2023-10-08 14:06:24 +0900

    Update:コントローラーの記述を簡素化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,15 +5,11 @@ class BookmarksController < ApplicationController
   def create
     hashed_url = params[:article_hashed_url]
     @article = Article.find_or_initialize_by(hashed_url: hashed_url)
-    if @article.new_record? || (@article.title.nil? && @article.content.nil? && @article.urlToImage.nil?)
-      @article.assign_attributes(url: params[:url],title: params[:title], content: params[:content], urlToImage: params[:urlToImage])
-      if @article.save
-        bookmark = Bookmark.find_or_create_by(user: current_user, article: @article)
-        flash[:success] = 'Article was successfully bookmarked.'
-      else
-        flash[:error] = 'There was an error bookmarking the article.'
-      end
-    else
+    if @article.new_record? 
+      @article.attributes = { url: params[:url], title: params[:title], content: params[:content], urlToImage: params[:urlToImage] }
+    end
+    
+    if @article.save
       if Bookmark.exists?(user: current_user, article: @article)
         flash[:notice] = 'Article is already bookmarked.'
       else
@@ -21,7 +17,6 @@ class BookmarksController < ApplicationController
         flash[:success] = 'Article was successfully bookmarked.'
       end
     end
-    
     redirect_to articles_path 
 end
 
