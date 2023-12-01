75bce97b09ee3150391475aef17cebbe93b0248c
hiroya0 <2000shigehiro0401@au.com>
2023-12-01 23:24:15 +0900

    Add:名前にバリデーションをかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true
+
   has_many :articles, through: :bookmarks
   has_many :bookmarks, dependent: :destroy
   has_many :comments, dependent: :destroy
