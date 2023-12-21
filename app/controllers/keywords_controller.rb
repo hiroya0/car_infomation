67fd32056b986b5d9753e9e69616ba946b95ff17
hiroya0 <2000shigehiro0401@au.com>
2023-12-21 23:06:44 +0900

    Add:フラッシュメッセージの編集

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -9,17 +9,17 @@ class KeywordsController < ApplicationController
   def create
     @keyword = current_user.keywords.build(keyword_params)
     if @keyword.save
-      redirect_to keywords_path, notice: 'キーワードが追加されました。'
+      redirect_to keywords_path, notice: t('keywords.add_success')
     else
       @keywords = current_user.keywords
-      render :index
+      redirect_to keywords_path, alert: t('keywords.delete_success')
     end
   end
 
   def destroy
     @keyword = current_user.keywords.find(params[:id])
     @keyword.destroy
-    redirect_to keywords_path, error: 'キーワードが削除されました。'
+    redirect_to keywords_path, alert: t('keywords.alert')
   end
 
   private
