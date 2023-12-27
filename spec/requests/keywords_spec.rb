772b6b50f0249d40523898796c604563a45b6124
hiroya0 <2000shigehiro0401@au.com>
2023-12-27 14:50:08 +0900

    Add:リクエストスペックを記述

--- spec/requests/keywords_spec.rb
+++ spec/requests/keywords_spec.rb
@@ -2,8 +2,55 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Keywords' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "キーワード", type: :request do
+  let(:user) { create(:user) }
+  let(:valid_word) { { word: 'テストキーワード' } }
+  let(:invalid_word) { { word: '' } }
+
+  before do
+    sign_in user
+  end
+
+  describe "POST /keywords" do
+    context "キーワードの入力が有効な場合" do
+      it "新しいキーワードを作成する" do
+        expect {
+          post keywords_path, params: { keyword: valid_word }
+        }.to change(Keyword, :count).by(1)
+      end
+
+      it "キーワード一覧にリダイレクトする" do
+        post keywords_path, params: { keyword: valid_word }
+        expect(response).to redirect_to(keywords_path)
+      end
+    end
+
+    context "キーワードの上限を超えた場合" do
+      before { 3.times { user.keywords.create!(valid_word) } }
+
+      it "新しいキーワードを作成しない" do
+        expect {
+          post keywords_path, params: { keyword: valid_word }
+        }.not_to change(Keyword, :count)
+      end
+
+      it "キーワード一覧にリダイレクトする" do
+        post keywords_path, params: { keyword: valid_word }
+        expect(response).to redirect_to(keywords_path)
+      end
+    end
+
+    context "何も入力がされていない場合" do
+      it "新しいキーワードを作成しない" do
+        expect {
+          post keywords_path, params: { keyword: invalid_word }
+        }.not_to change(Keyword, :count)
+      end
+
+      it "キーワード一覧にリダイレクトする" do
+        post keywords_path, params: { keyword: invalid_word }
+        expect(response).to redirect_to(keywords_path)
+      end
+    end
   end
 end
